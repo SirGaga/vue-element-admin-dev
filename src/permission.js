7dc715c8dd16ad10cd5a060fc9788b615b18f7a9
family <214151436@qq.com>
2020-11-18 17:57:58 +0800

    addcomment

--- src/permission.js
+++ src/permission.js
@@ -1,32 +1,32 @@
 import router from './router'
 import store from './store'
 import { Message } from 'element-ui'
-import NProgress from 'nprogress' // progress bar
-import 'nprogress/nprogress.css' // progress bar style
-import { getToken } from '@/utils/auth' // get token from cookie
-import getPageTitle from '@/utils/get-page-title'
+import NProgress from 'nprogress' // 进度条
+import 'nprogress/nprogress.css' // 进度条央视
+import { getToken } from '@/utils/auth' // 从cookie中获取token，以后要使用后台传送过来的jwt
+import getPageTitle from '@/utils/get-page-title'// 获取页面title
 
-NProgress.configure({ showSpinner: false }) // NProgress Configuration
+NProgress.configure({ showSpinner: false }) // 进度条基本配置
 
-const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
-
+const whiteList = ['/login', '/auth-redirect'] // 不用重定向的白名单，可以在浏览器地址栏直接使用该url
+// 对每一次请求都会进行一次过滤
 router.beforeEach(async(to, from, next) => {
-  // start progress bar
+  // 开始使用进度条
   NProgress.start()
 
-  // set page title
+  // 设置dom的title，保证每一次页面跳转都能获取title
   document.title = getPageTitle(to.meta.title)
 
-  // determine whether the user has logged in
+  // 通过获取token来判断用户是否登录
   const hasToken = getToken()
 
   if (hasToken) {
     if (to.path === '/login') {
-      // if is logged in, redirect to the home page
+      // 如果已经登录还去login的话直接跳转到 / 路径下
       next({ path: '/' })
       NProgress.done() // hack: https://github.com/PanJiaChen/vue-element-admin/pull/2939
     } else {
-      // determine whether the user has obtained his permission roles through getInfo
+      // 判断该用户是否已经获取其权限（该权限需要提前和path提前进行匹配）
       const hasRoles = store.getters.roles && store.getters.roles.length > 0
       if (hasRoles) {
         next()
